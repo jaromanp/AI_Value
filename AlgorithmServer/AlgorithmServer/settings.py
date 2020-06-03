1b45b955ea8bd7e19b5c3bf146c180150673eac0
jaromanp <33042735+jaromanp@users.noreply.github.com>
2020-06-03 10:24:50 -0500

    Server

--- AlgorithmServer/AlgorithmServer/settings.py
+++ AlgorithmServer/AlgorithmServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@7)e9r!-zjv-2n3e)gaoy%)wa9fwu^@rgniypf2_n024_ax_k)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apiserver',
 ]
 
 MIDDLEWARE = [
