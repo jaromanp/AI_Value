d1d31387685af5812e3f6fb1d2b01139e4f98a2d
EC2 Default User <ec2-user@ip-172-31-52-70.ec2.internal>
2020-06-04 00:35:50 +0000

    Se desecha la posibilidad de flask

--- server-flask/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-response = requests.post('http://localhost:8000/process',
-                         headers={'Content-Type': 'application/json'},
-                         data=json.dumps({'data':'Request'}))
-print(response.json())
